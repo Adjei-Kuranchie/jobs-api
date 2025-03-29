35c1f473336a00a2a7871597ce46f742711bbe72
Adjei-Kuranchie <yaw.kuranchie@stu.ucc.edu.gh>
2025-03-29 21:46:15 +0000

    enhance authentication middleware to verify JWT and attach user information to request

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -4,9 +4,24 @@ const { UnauthenticatedError } = require("../errors");
 
 const auth = function (req, res, next) {
   //check for header
-
   const authHeader = req.headers.authorization;
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
     throw new UnauthenticatedError("Authentication invalid");
   }
+
+  //get token from header
+  const token = authHeader.split(" ")[1];
+
+  //verify token
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+
+    // Attach the user to the job routes
+    req.user = { userId: payload.userId, name: payload.name };
+    next();
+  } catch (error) {
+    throw new UnauthenticatedError("Authentication invalid");
+  }
 };
+
+module.exports = auth;
