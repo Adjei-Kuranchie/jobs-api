653afd11de362ca6cca8eb466684f63799a36b19
Adjei-Kuranchie <yaw.kuranchie@stu.ucc.edu.gh>
2025-03-29 22:27:05 +0000

    add job model and implement job management routes with authentication

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,21 +1,27 @@
+const Job = require("../models/Job");
+const { StatusCodes } = require("http-status-codes");
+const { BadRequestError, NotFoundError } = require("../errors");
+
 const getAllJobs = async (req, res) => {
-  res.send("get all jobs");
+  res.json({ msg: "get all jobs" });
 };
 
 const getJob = async (req, res) => {
-  res.send("get jobs");
+  res.json({ msg: "get jobs" });
 };
 
 const createJob = async (req, res) => {
-  res.send("create job");
+  req.body.createdBy = req.user.userId;
+  const job = await Job.create(req.body);
+  res.status(StatusCodes.CREATED).json({ job });
 };
 
 const updateJob = async (req, res) => {
-  res.send("update job");
+  res.json({ msg: "update job" });
 };
 
 const deleteJob = async (req, res) => {
-  res.send("delete job");
+  res.json({ msg: "delete job" });
 };
 
 module.exports = {
--- middleware/authentication.js
+++ middleware/authentication.js
@@ -6,21 +6,22 @@ const auth = function (req, res, next) {
   //check for header
   const authHeader = req.headers.authorization;
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    throw new UnauthenticatedError("Authentication invalid");
+    throw new UnauthenticatedError("Authentication invalid :No auth header");
   }
 
   //get token from header
   const token = authHeader.split(" ")[1];
+  console.log(token);
 
   //verify token
   try {
-    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    const payload = jwt.verify(token, process.env.JWT_TOKEN);
 
     // Attach the user to the job routes
     req.user = { userId: payload.userId, name: payload.name };
     next();
   } catch (error) {
-    throw new UnauthenticatedError("Authentication invalid");
+    throw new UnauthenticatedError("Authentication invalid :Invalid token");
   }
 };
 
