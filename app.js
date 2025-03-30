95ac39710ac35f94d6fa988fde16f22bf6636fa4
Adjei-Kuranchie <yaw.kuranchie@stu.ucc.edu.gh>
2025-03-30 20:48:55 +0000

    add rate limiter middleware to limit requests and enhance API security

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
 
 const authRouter = require("./routes/auth");
 const jobsRouter = require("./routes/jobs");
@@ -24,6 +25,12 @@ app.use(express.json());
 app.use(helmet());
 app.use(cors());
 app.use(xss());
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+);
 
 // routes
 app.use("/api/v1/auth", authRouter);
