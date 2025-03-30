3b8b9d4d0a8d3a94748aca58368c36093383784a
Adjei-Kuranchie <yaw.kuranchie@stu.ucc.edu.gh>
2025-03-30 20:48:17 +0000

    add xss-clean middleware to prevent cross-site scripting attacks

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const express = require("express");
 // security packages
 const helmet = require("helmet");
 const cors = require("cors");
+const xss = require("xss-clean");
 
 const authRouter = require("./routes/auth");
 const jobsRouter = require("./routes/jobs");
@@ -22,6 +23,7 @@ app.use(express.json());
 // extra packages
 app.use(helmet());
 app.use(cors());
+app.use(xss());
 
 // routes
 app.use("/api/v1/auth", authRouter);
